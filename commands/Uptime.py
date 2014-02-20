95ddd4f757d71f69a7b1a190c9343eed64afdf9e
Víctor Rodríguez <victoratvrdominguez.es>
2014-02-20 17:57:58 +0100

    Uptime bugfix: Now its displayed correctly

--- commands/Uptime.py
+++ commands/Uptime.py
@@ -4,12 +4,13 @@ import subprocess as s
 class Uptime(Command):
 	'Get system uptime'
 	def runCommand(self):
-		uptime = s.Popen(['/usr/bin/uptime'] , stdout=s.PIPE, stderr=s.PIPE).communicate()[0].split("\n")[0].split()
+		uptime = s.Popen(['/usr/bin/uptime'] , stdout=s.PIPE, stderr=s.PIPE).communicate()[0].split("\n")[0].replace(",","").split()
+		
 		
 		if uptime[3] == 'day' or uptime[3] == 'days':
-			time = uptime[4].plsit(':')
+			time = uptime[4].split(':')
 			return ['    UPTIME', '--------------', 'Days: ' + uptime[2], 'Hours: ' + time[0], 'Minutes: ' + time[1]]
-		elif uptime[3] == 'min' or uptime[3] == 'min,':
+		elif uptime[3] == 'min':
 			return ['    UPTIME', '--------------', 'Days: 0', 'Hours: 0',  'Minutes: ' + uptime[2]]
 		else:
 			time = uptime[2].split(':')
