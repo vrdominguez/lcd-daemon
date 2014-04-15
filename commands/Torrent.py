7fa85bf156311e368d369e59c327497cad91a901
Víctor Rodríguez <victor[at]vrdominguez.es>
2014-04-15 17:00:01 +0200

    Added transmission-daemon status check before the client start

--- commands/Torrent.py
+++ commands/Torrent.py
@@ -1,11 +1,20 @@
 from Command import Command
 from transmission import Transmission # pip install transmission-fluid
+import subprocess as s
 
 class Torrent(Command):
 	'Get transmission stats'
 	def runCommand(self):
-		config = self.loadConfig('transmission');
-		client = Transmission(username=config['user'], password=config['passwd'])
-		response = client('session-stats')
 		
-		return [' TRANSMISSION','--------------','Acive: ' + str(response['activeTorrentCount']),'Speeds (kbps)','- Up:' + str(response['uploadSpeed']/1024),'- Down:' + str(response['downloadSpeed']/1024)]
+		# Check if transmission is running
+		transmission_status = s.Popen(['/usr/sbin/service', 'transmission-daemon', 'status'] , stdout=s.PIPE, stderr=s.PIPE).communicate()[0].split("\n")[0]
+		
+		if transmission_status == 'transmission-daemon is running.':
+			config = self.loadConfig('transmission');
+			client = Transmission(username=config['user'], password=config['passwd'])
+			response = client('session-stats')
+			
+			return [' TRANSMISSION','--------------','Active: ' + str(response['activeTorrentCount']),'Speeds (KB/s)','- Up: ' + str(response['uploadSpeed']/1024),'- Down: ' + str(response['downloadSpeed']/1024)]
+		
+		else:
+			return [' TRANSMISSION','--------------','', ' NOT RUNNING!']
